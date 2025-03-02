1194b8b29d42e07d859f21852e996a771a11a0f5
Quocduyctvn <quocduyctvn@gmail.com>
2025-03-02 15:08:03 +0700

    init

---
+++ EcoShrimp.Client/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EcoShrimp.Client.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
