33eb0e68a3fad960c1c5f7a7ccfb0862a871977d
Quocduyctvn <quocduyctvn@gmail.com>
2025-03-03 18:00:41 +0700

    Add: IsInPermission method for ClaimsPrincipal

---
+++ EcoShrimp.Share/Extension/AppExtension.cs
@@ -0,0 +1,35 @@
+﻿using EcoShrimp.Share.Const;
+using System.Security.Claims;
+
+namespace EcoShrimp.Share.Extension
+{
+	/// <summary>
+	/// App extension
+	/// </summary>
+	public static class AppExtension
+	{
+
+		public static bool IsInPermission(this ClaimsPrincipal user, int actionPermission)
+		{
+			// Find the user’s permissions claim
+			var userPermission = user?.FindFirst(AppClaimTypes.Permissions);
+
+			// Return false if the permission claim is not present or its value is empty
+			if (userPermission == null || string.IsNullOrEmpty(userPermission.Value))
+			{
+				return false;
+			}
+
+			// If the user has no permissions (special case), always return true
+			if (actionPermission == AuthConst.NO_PERMISSION)
+			{
+				return true;
+			}
+
+			// Check if the user’s permissions contain the specified actionPermission (in string format)
+			return userPermission.Value.Split(',').Any(permission => permission.Trim() == actionPermission.ToString());
+		}
+
+	}
+}
+
