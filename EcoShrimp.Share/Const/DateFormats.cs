2cb1c8387732bbc9a4bb47c79e2132c7251d1c05
Quocduyctvn <quocduyctvn@gmail.com>
2025-03-04 12:41:47 +0700

    Add DateFormats constant

---
+++ EcoShrimp.Share/Const/DateFormats.cs
@@ -0,0 +1,12 @@
+﻿namespace EcoShrimp.Share.Const
+{
+	/// <summary>
+	/// date formats
+	/// </summary>
+	public class DateFormats
+	{
+		public const string DATE_ONLY = "dd/MM/yyyy";
+		public const string DATETIME = "dd/MM/yyyy HH:mm";
+		public const string TIME_ONLY = "HH:mm";
+	}
+}
