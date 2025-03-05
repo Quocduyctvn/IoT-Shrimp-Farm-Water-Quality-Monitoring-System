9f31605d28b11b2801e9d21a5d67c990785b4f5c
Quocduyctvn <quocduyctvn@gmail.com>
2025-03-05 08:10:53 +0700

    Add message constant

---
+++ EcoShrimp.Share/Const/MessageConst.cs
@@ -0,0 +1,12 @@
+﻿namespace EcoShrimp.Share.Const
+{
+	/// <summary>
+	/// message const
+	/// </summary>
+	public class MessageConst
+	{
+		public const string REQUIRED = "Vui lòng nhập trường này!";
+		public const string VALID_NUMBER_REQUIRED = "Vui lòng nhập một số hợp lệ.";
+		public const string GREATER_EQUAL_ZERO = "Vui lòng nhập giá trị lớn hơn hoặc bằng 0.";
+	}
+}
