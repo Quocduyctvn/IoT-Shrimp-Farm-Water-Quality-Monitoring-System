627551c212ded5346abd1be83ee57f45c70cdcda
Quocduyctvn <quocduyctvn@gmail.com>
2025-03-11 21:30:37 +0700

    Add enum ConnectStatus for RaspberryPi

---
+++ EcoShrimp.Share/Enums/ConnectStatus.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcoShrimp.Share.Enums
+{
+	/// <summary>
+	/// Represents the status of a connection.
+	/// </summary>
+	public enum ConnectStatus
+	{
+		[Display(Name = "Chưa kết nối")]
+		NotConnected = 0,
+
+		[Display(Name = "Kết nối thành công")]
+		Connected = 2,
+
+		[Display(Name = "Kết nối bị gián đoạn")]
+		Disconnected = 3
+	}
+}
