0d45c4fe1b89aec3583ff2cb8f96ecb94b9d59e2
Quocduyctvn <quocduyctvn@gmail.com>
2025-03-11 21:28:58 +0700

    Add: enum MaxLength

---
+++ EcoShrimp.Share/Enums/MaxLength.cs
@@ -0,0 +1,24 @@
+﻿namespace EcoShrimp.Share.Enums
+{
+	/// <summary>
+	/// Mode to validate
+	/// </summary>
+	public enum MaxLength
+	{
+		MAX_8 = 8,
+		MAX_16 = 16,
+		MAX_32 = 32,
+		MAX_64 = 64,
+		MAX_96 = 96,
+		MAX_100 = 100,
+		MAX_128 = 128,
+		MAX_255 = 255,
+		MAX_256 = 256,
+		MAX_500 = 500,
+		MAX_512 = 512,
+		MAX_1024 = 1024,    // 1 KB
+		MAX_2048 = 2048,
+		MAX_4096 = 4096,     // 4 KB
+		MAX_8192 = 8192
+	}
+}
