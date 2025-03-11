2e77f0c9df4b938a113d122e4826f5ba1270984a
Quocduyctvn <quocduyctvn@gmail.com>
2025-03-11 21:31:24 +0700

    Update enum Status

---
+++ EcoShrimp.Share/Enums/Status.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcoShrimp.Share.Enums
+{
+	public enum Status
+	{
+		[Display(Name = "Hoạt động")]
+		Active = 1,
+
+		[Display(Name = "Không hoạt động")]
+		Inactive = 2,
+
+		[Display(Name = "Đã xóa")]
+		Deleted = 3,
+
+		[Display(Name = "Đã bán")]
+		Bought = 4
+	}
+}
