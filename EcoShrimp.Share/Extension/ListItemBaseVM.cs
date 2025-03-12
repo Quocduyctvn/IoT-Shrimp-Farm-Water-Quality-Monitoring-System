8767c3e28052ab3d7b4bd1d4f81f7dd5d7c640c8
Quocduyctvn <quocduyctvn@gmail.com>
2025-03-12 16:11:28 +0700

    Update ListItemBaseVM

---
+++ EcoShrimp.Share/Extension/ListItemBaseVM.cs
@@ -0,0 +1,11 @@
+﻿namespace EcoShrimp.Share.Extension
+{
+	/// <summary>
+	/// Base class for list item
+	/// </summary>
+	public class ListItemBaseVM
+	{
+		public int Id { get; set; }
+		public int RowIndex { get; set; }
+	}
+}
