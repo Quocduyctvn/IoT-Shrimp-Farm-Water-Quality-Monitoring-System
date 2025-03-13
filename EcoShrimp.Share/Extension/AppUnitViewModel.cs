202e9d715a81874bbea95264a85b606503e4f349
Quocduyctvn <quocduyctvn@gmail.com>
2025-03-13 07:40:50 +0700

    Create App UnitViewModel

---
+++ EcoShrimp.Share/Extension/AppUnitViewModel.cs
@@ -0,0 +1,20 @@
+﻿using EcoShrimp.Share.Enums;
+using Microsoft.AspNetCore.Mvc.Rendering;
+namespace EcoShrimp.Share.Extension
+{
+	public class AppUnitViewModel
+	{
+		public Status Status { get; set; }
+		public IEnumerable<SelectListItem> StatusList { get; set; }
+
+		public AppUnitViewModel()
+		{
+			StatusList = new List<SelectListItem>
+		{
+			new SelectListItem { Value = ((int)Status.Active).ToString(), Text = "Hoạt động" },
+			new SelectListItem { Value = ((int)Status.Inactive).ToString(), Text = "Tạm ngưng" },
+			new SelectListItem { Value = ((int)Status.Deleted).ToString(), Text = "Đã xóa" }
+		};
+		}
+	}
+}
