614f30f36264363193a3f375a19e02b1d728aae6
Quocduyctvn <quocduyctvn@gmail.com>
2025-03-23 17:56:51 +0700

    Add table AppCategories

---
+++ EcoShrimp.Data/Entities/AppCategories.cs
@@ -0,0 +1,14 @@
+﻿using EcoShrimp.Data.Entities.Base;
+using EcoShrimp.Share.Enums;
+
+namespace EcoShrimp.Data.Entities
+{
+	public class AppCategories : AppEntityBase
+	{
+		public string Name { get; set; }
+		public string? Desc { get; set; }
+		public Status Status { get; set; }
+		public int SortOrder { get; set; }
+		public ICollection<AppProducts> appProducts { get; set; }
+	}
+}
