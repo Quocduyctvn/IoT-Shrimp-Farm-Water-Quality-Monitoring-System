62d036e78b58f03e5c9b813faaa46359c8e82969
Quocduyctvn <quocduyctvn@gmail.com>
2025-03-23 17:58:08 +0700

    Add entity AppPonds

---
+++ EcoShrimp.Data/Entities/AppPonds.cs
@@ -0,0 +1,32 @@
+﻿using EcoShrimp.Data.Entities.Base;
+using EcoShrimp.Share.Enums;
+
+namespace EcoShrimp.Data.Entities
+{
+	/// <summary>
+	/// Represents a shrimp pond in the system.
+	/// </summary>
+	public class AppPonds : AppEntityBase
+	{
+		public AppPonds()
+		{
+			appSeasons = new HashSet<AppSeasons>();
+		}
+
+		public string Name { get; set; }
+		public string? Address { get; set; }
+		// diện tích ao
+		public decimal? Area { get; set; }
+		// Active, Inactive
+		public Status Status { get; set; }
+		public string? Desc { get; set; }
+		public int SortOrder { get; set; }
+
+		//-----------------
+		public int IdFarm { get; set; }
+		public AppFarms appFarm { get; set; }
+		//-----------------
+		public ICollection<AppSeasons> appSeasons { get; set; }
+
+	}
+}
