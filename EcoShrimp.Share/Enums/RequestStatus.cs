0eff7d6299938141444a801793abc4c58299f7b2
Quocduyctvn <quocduyctvn@gmail.com>
2025-03-31 19:30:07 +0700

    Add enum Request Status

---
+++ EcoShrimp.Share/Enums/RequestStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace EcoShrimp.Share.Enums
+{
+	public enum RequestStatus
+	{
+		Pending = 1,      // Đang chờ xử lý
+		Resolved = 2,     // Đã giải quyết
+		Rejected = 3,     // Bị từ chối
+		Deleted = 4
+	}
+}
