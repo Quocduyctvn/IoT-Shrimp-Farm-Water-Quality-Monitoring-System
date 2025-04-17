f96365d0b135dfb6492ade38ccf163cfc16abe7d
Quocduyctvn <quocduyctvn@gmail.com>
2025-04-17 20:30:19 +0700

    add Permissions entity

---
+++ EcoShrimp.Data/Entities/AppPermissions.cs
@@ -0,0 +1,14 @@
+﻿using EcoShrimp.Data.Entities.Base;
+
+namespace EcoShrimp.Data.Entities
+{
+	public class AppPermissions : AppEntityBase
+	{
+		public string Code { get; set; }
+		public string Table { get; set; }
+		public string GroupName { get; set; }
+		public string Desc { get; set; }
+
+		public ICollection<AppRolePermission> appRolePermissions { get; set; }
+	}
+}
