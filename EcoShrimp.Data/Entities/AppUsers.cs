110e89da2d54f69bd1aa2ded8a68e623d5ec3ea7
Quocduyctvn <quocduyctvn@gmail.com>
2025-04-17 20:33:17 +0700

    add User entity

---
+++ EcoShrimp.Data/Entities/AppUsers.cs
@@ -0,0 +1,20 @@
+﻿using EcoShrimp.Data.Entities.Base;
+using EcoShrimp.Share.Enums;
+
+namespace EcoShrimp.Data.Entities
+{
+	public class AppUsers : AppEntityBase
+	{
+		public string Name { get; set; }
+		public string? Address { get; set; }
+		public string? Email { get; set; }
+		public string Phone { get; set; }
+		public string Pass { get; set; }
+		public string? Avatar { get; set; }
+		public Status Status { get; set; }
+		public int? SortOrder { get; set; }
+		//---------------------------------------
+		public int IdRole { get; set; }
+		public AppRoles appRole { get; set; }
+	}
+}
