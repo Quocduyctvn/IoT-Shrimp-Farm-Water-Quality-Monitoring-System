b72b1d50bb847295c7bd371ce20b3e4c8766cbc9
Quocduyctvn <quocduyctvn@gmail.com>
2025-04-17 20:33:03 +0700

    add Threhold entity

---
+++ EcoShrimp.Data/Entities/AppThresholds.cs
@@ -0,0 +1,16 @@
+﻿using EcoShrimp.Data.Entities.Base;
+
+namespace EcoShrimp.Data.Entities
+{
+	public class AppThresholds : AppEntityBase
+	{
+		public string Name { get; set; }
+		public string Unit { get; set; }
+		public double Min { get; set; }
+		public double Max { get; set; }
+		public string? Desc { get; set; }
+
+		public int IdFarm { get; set; }
+		public AppFarms appFarms { get; set; }
+	}
+}
