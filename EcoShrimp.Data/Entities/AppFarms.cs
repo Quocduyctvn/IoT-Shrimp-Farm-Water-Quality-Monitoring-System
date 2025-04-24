fa498f7512128802b8f831b948d370023ba24ef0
Quocduyctvn <quocduyctvn@gmail.com>
2025-04-24 10:17:40 +0700

    add property notify message

--- EcoShrimp.Data/Entities/AppFarms.cs
+++ EcoShrimp.Data/Entities/AppFarms.cs
@@ -28,6 +28,9 @@ namespace EcoShrimp.Data.Entities
 		public string? Desc { get; set; }
 		public Status Status { get; set; }
 		public int SortOrder { get; set; }
+		public bool IsNotify { get; set; }
+		// ----------------------
+		public int? IdTime { get; set; }
 		//----------------------  
 		public ICollection<AppPonds> appPonds { get; set; }
 		public ICollection<AppProInstances> appProInstances { get; set; }
